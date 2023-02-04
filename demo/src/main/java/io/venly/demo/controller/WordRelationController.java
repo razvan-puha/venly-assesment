d3bafbe21116665aa7fbfdbb65b0ade4266c90f4
Puha Razvan Petru <puha.razvan@192-168-0-107.rdsnet.ro>
2023-02-04 22:09:00 +0200

    Add OpenAPI description

--- demo/src/main/java/io/venly/demo/controller/WordRelationController.java
+++ demo/src/main/java/io/venly/demo/controller/WordRelationController.java
@@ -1,8 +1,18 @@
 package io.venly.demo.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterStyle;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.SchemaProperty;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.venly.demo.dto.WordRelationDto;
 import io.venly.demo.dto.WordRelationRequestBody;
 import io.venly.demo.entity.RelationType;
+import io.venly.demo.entity.WordRelation;
 import io.venly.demo.exception.ResourceCreatedException;
 import io.venly.demo.service.WordRelationService;
 import jakarta.validation.Valid;
@@ -22,16 +32,47 @@ import java.util.List;
 public class WordRelationController {
     WordRelationService wordRelationService;
 
+    @Operation(summary = "Add a relation between two words")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "201",
+            description = "The relation was added to the database"
+        ),
+        @ApiResponse(responseCode = "400",
+            description = "The relation already exists",
+            content = @Content(mediaType = MediaType.TEXT_PLAIN_VALUE)
+        )
+    })
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
-    public void addWordRelation(@RequestBody @Valid WordRelationRequestBody requestBody) {
+    public void addWordRelation(
+        @io.swagger.v3.oas.annotations.parameters.RequestBody(
+            required = true,
+            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = WordRelationRequestBody.class))
+        )
+        @RequestBody
+        @Valid
+        WordRelationRequestBody requestBody
+    ) {
         int newResourceId = wordRelationService.addWordRelation(requestBody);
         throw new ResourceCreatedException(URI.create(String.format("/api/word_relations/%d", newResourceId)));
     }
 
+    @Operation(summary = "Get the list of all relations")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200",
+            description = "The list of stored relations",
+            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                array = @ArraySchema(schema = @Schema(implementation = WordRelationDto.class))
+            )
+        )
+    })
     @GetMapping
     public List<WordRelationDto> getWordRelations(
-        @RequestParam(value = "relation", required = false) String relation,
-        @RequestParam(value = "inverse", required = false) boolean showAlsoInverse
+        @Parameter(description = "Filter by the relation type")
+        @RequestParam(value = "relation", required = false)
+        String relation,
+        @Parameter(description = "Show also the reversed relations")
+        @RequestParam(value = "inverse", required = false)
+        boolean showAlsoInverse
     ) {
         if (relation == null || relation.isEmpty()) {
             return wordRelationService.getWordRelations();
@@ -40,10 +81,32 @@ public class WordRelationController {
         }
     }
 
-    @GetMapping(value = "/search",consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @Operation(
+        summary = "Create a search path between the source and the target",
+        requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+            content = @Content(mediaType = MediaType.MULTIPART_FORM_DATA_VALUE)
+        )
+    )
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200",
+            description = "The result path between the source and the target",
+            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                array = @ArraySchema(schema = @Schema(implementation = String.class))
+            )
+        ),
+        @ApiResponse(responseCode = "400",
+            description = "No path has been found between the source and the target",
+            content = @Content(mediaType = MediaType.TEXT_PLAIN_VALUE)
+        )
+    })
+    @GetMapping(value = "/search", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public List<String> pathSearch(
-        @RequestPart(value = "source", required = false) String source,
-        @RequestPart(value = "target", required = false) String target
+        @Parameter(description = "The start word of the path")
+        @RequestPart(value = "source")
+        String source,
+        @Parameter(description = "The target word of the path")
+        @RequestPart(value = "target")
+        String target
     ) {
         return wordRelationService.getPath(source.toLowerCase().trim(), target.toLowerCase().trim());
     }
